33a5cefc89b4080536a928b9c66ad0a3d137941b
Fighter1125 <1414589221@qq.com>
2018-11-22 23:04:10 +0800

    Code07: 轮播子组件

---
+++ chen-shop/src/components/subcomponents/Swipe.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <mt-swipe :auto="4000" ref="swipe" @change="">
+            <mt-swipe-item><img src="../../images/l1.jpg" alt=""></mt-swipe-item>
+            <mt-swipe-item><img src="../../images/l2.jpg" alt=""></mt-swipe-item>
+            <mt-swipe-item><img src="../../images/l3.jpg" alt=""></mt-swipe-item>
+            <mt-swipe-item><img src="../../images/l4.jpg" alt=""></mt-swipe-item>
+        </mt-swipe>
+        <!--<button @click="prev">pre</button>-->
+        <!--<button @click="next">next</button>-->
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Swipe",
+        data(){},
+        methods: {
+
+            //控制轮播图切换下一张
+            next(){
+                this.$refs.swipe.next()
+            },
+
+            //控制轮播图切换上一张
+            prev(){
+                console.log(this.$refs.swipe)
+                this.$refs.swipe.prev();
+            },
+        },
+    }
+</script>
+
+<style lang="less" scoped>
+    /*轮播图*/
+    .mint-swipe {
+        height: 250px;
+        @media screen and (min-width: 500px) {
+            & {
+                height: 350px;
+            }
+        }
+        @media screen and (min-width: 900px) {
+            & {
+                height: 550px;
+            }
+        }
+        .mint-swipe-item {
+            img {
+                height: 100%;
+                width: 100%;
+            }
+        }
+    }
+</style>
