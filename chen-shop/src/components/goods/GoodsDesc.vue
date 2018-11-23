60a0f356be76eefc4ceca8bbc8242fbba772a367
Fighter1125 <1414589221@qq.com>
2018-11-23 21:43:21 +0800

    Code08: 商品图文详情组件

---
+++ chen-shop/src/components/goods/GoodsDesc.vue
@@ -0,0 +1,28 @@
+<template>
+    <h1>商品介绍 id===={{goodsId}}</h1>
+</template>
+
+<script>
+    export default {
+        name: "GoodsDesc",
+        data(){
+            return {
+                goodsId: null,
+                info: null,
+            }
+        },
+        created(){
+            this.goodsId=this.$route.params.id;
+        },
+        methods: {
+            // 根据id获取详情
+            getGoodsDesc(){
+
+            }
+        },
+    }
+</script>
+
+<style scoped>
+
+</style>
