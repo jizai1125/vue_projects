9ec833d990e14a25a5f65cd3e6fefd6b452b202f
Fighter1125 <1414589221@qq.com>
2018-11-23 21:42:38 +0800

    Code08: 商品评论组件

---
+++ chen-shop/src/components/goods/GoodsCmts.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <comment :id="this.goodsId"></comment>
+    </div>
+</template>
+
+<script>
+    import comment from '../subcomponents/Comment.vue';
+    export default {
+        data(){
+            return {
+                goodsId: null,
+            }
+        },
+        created(){
+            this.goodsId=this.$route.params.id;
+        },
+        methods: {
+            // 根据id获取评论
+            getComments(){
+
+            }
+        },
+        components: {
+            comment
+        }
+    }
+</script>
+
+<style lang="less" scoped>
+
+</style>
