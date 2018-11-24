b12554ee3ace23bdec255fc6e8122ddf39472490
Fighter1125 <1414589221@qq.com>
2018-11-24 18:36:12 +0800

    Code09: 购物车数字选择框子组件

---
+++ chen-shop/src/components/subcomponents/Numbox_shopCar.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="mui-numbox" style="height: 30px;">
+        <button class="mui-btn mui-numbox-btn-minus">-</button>
+        <input class="mui-numbox-input" type="number" ref="count" @change="countChange"/>
+        <button class="mui-btn mui-numbox-btn-plus">+</button>
+    </div>
+</template>
+
+<script>
+    import mui from '../../lib/mui/js/mui.js';
+
+    export default {
+        name: "Numbox_shopCar",
+        data(){
+            return {}
+        },
+        mounted(){
+            //初始化数字输入框，一定要在mounted方法里调用，否则会报错哦，因为要等元素挂载到DOM树，选择器才能选择到
+            mui('.mui-numbox').numbox().setValue(1);
+        },
+        methods: {
+            countChange(){
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
