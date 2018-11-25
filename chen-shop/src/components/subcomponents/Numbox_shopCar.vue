99b70c9f124c2582be2584102c758917f5da37d1
Fighter1125 <1414589221@qq.com>
2018-11-25 23:23:58 +0800

    Code10: 数字选择框组件（购物车）

--- chen-shop/src/components/subcomponents/Numbox_shopCar.vue
+++ chen-shop/src/components/subcomponents/Numbox_shopCar.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="mui-numbox" style="height: 30px;">
         <button class="mui-btn mui-numbox-btn-minus">-</button>
-        <input class="mui-numbox-input" type="number" ref="count" @change="countChange"/>
+        <input class="mui-numbox-input" ref="numbox" readonly type="number" :value="count" @change="countChange"/>
         <button class="mui-btn mui-numbox-btn-plus">+</button>
     </div>
 </template>
@@ -10,16 +10,25 @@
     import mui from '../../lib/mui/js/mui.js';
 
     export default {
-        name: "Numbox_shopCar",
+        props: ['count','goodsId'],
         data(){
             return {}
         },
         mounted(){
             //初始化数字输入框，一定要在mounted方法里调用，否则会报错哦，因为要等元素挂载到DOM树，选择器才能选择到
-            mui('.mui-numbox').numbox().setValue(1);
+            mui('.mui-numbox').numbox();
         },
         methods: {
             countChange(){
+                //当数量count为0时，从购物车钩删除商品
+                let count=parseInt(this.$refs.numbox.value);
+                if(count<=0){
+                    this.$store.commit('removeGoods',this.goodsId)
+                }
+                this.$store.commit('updateShopCar',{
+                    id: this.goodsId,
+                    count: this.$refs.numbox.value
+                })
             }
         }
     }
