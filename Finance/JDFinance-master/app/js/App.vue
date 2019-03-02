b34d10f53e1a06a9c521b8b698ad247cf95019da
Fighter1125 <1414589221@qq.com>
2019-03-02 09:58:50 +0800

    主组件

---
+++ Finance/JDFinance-master/app/js/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="app">
+        <router-view/>
+        <Navbar/>
+    </div>
+</template>
+
+<script>
+import Navbar from "./public/navbar.vue"
+
+export default {
+    name: "App",
+    components: {
+        Navbar,
+    },
+}
+</script>
