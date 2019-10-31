b4eb4e05590d4748a2159606993b5f1187a1962f
chen <1414589221@qq.com>
2019-10-31 20:22:59 +0800

    complete singerInfo component

--- vue-music/src/components/music-list/music-list.vue
+++ vue-music/src/components/music-list/music-list.vue
@@ -1,12 +1,19 @@
 <template>
   <div class="music-list">
-    <div class="back">
+    <div class="back" @click="back">
       <i class="icon-back" />
     </div>
     <h1 class="title" v-html="title" />
     <div ref="bgImage" class="bg-image" :style="bgStyle">
-      <div class="filter" />
+      <div v-show="songList.length > 0" ref="playBtn" class="play-wrapper">
+        <div class="play">
+          <i class="icon-play" />
+          <span class="text">随机播放全部</span>
+        </div>
+      </div>
+      <div ref="filter" class="filter" />
     </div>
+    <div ref="bgLayer" class="bg-layer" />
     <!-- 歌曲列表 -->
     <Scroll
       ref="list"
@@ -53,7 +60,7 @@ export default {
   },
   data() {
     return {
-
+      scrollY: 0
     }
   },
   computed: {
@@ -61,16 +68,53 @@ export default {
       return `background-image: url(${this.bgImage})`
     }
   },
+  watch: {
+    scrollY(newY) {
+      const diff = this.bgImageHeight + newY
+      let zIndex = 0
+      let scale = 1
+      let blur = 0
+      this.$refs.bgLayer.style.transform = `translate3d(0, ${newY}px, 0)`
+      this.$refs.bgLayer.style.webkitTransform = `translate3d(0, ${newY}px, 0)`
+      const percent = Math.abs(newY / this.bgImageHeight)
+      if (newY > 0) {
+        zIndex = 10
+        scale = 1 + percent
+      } else {
+        blur = Math.min(20 * percent, 20)
+      }
+      this.$refs.filter.style.backdropFilter = `blur(${blur}px)`
+      this.$refs.filter.style.webkitBackdropFilter = `blur(${blur}px)`
+      if (diff <= this.minTranslateY) {
+        zIndex = 10
+        this.$refs.bgImage.style.height = `${this.minTranslateY}px`
+        this.$refs.bgImage.style.paddingTop = 0
+        this.$refs.playBtn.style.display = 'none'
+      } else {
+        this.$refs.bgImage.style.height = 0
+        this.$refs.bgImage.style.paddingTop = '70%'
+        this.$refs.playBtn.style.display = ''
+      }
+      this.$refs.bgImage.style.zIndex = zIndex
+      this.$refs.bgImage.style.transform = `scale(${scale})`
+      this.$refs.bgImage.style.webkitTransform = `scale(${scale})`
+    }
+  },
   created() {
     this.listenScroll = true
     this.probeType = 3
   },
   mounted() {
-    this.$refs.list.$el.style.top = `${this.$refs.bgImage.clientHeight}px`
+    this.bgImageHeight = this.$refs.bgImage.clientHeight
+    this.minTranslateY = 40
+    this.$refs.list.$el.style.top = `${this.bgImageHeight}px`
   },
   methods: {
-    scroll(e) {
-      console.log(e)
+    scroll(pos) {
+      this.scrollY = pos.y
+    },
+    back() {
+      this.$router.back()
     }
   }
 }
