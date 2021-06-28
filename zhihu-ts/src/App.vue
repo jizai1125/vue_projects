7df11a80c5396247393012969167146df9c268b0
jizai1125 <1414589221@qq.com>
2021-06-28 23:30:00 +0800

    add: dropdown.vue

--- zhihu-ts/src/App.vue
+++ zhihu-ts/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="container">
+    <global-header :user="user"></global-header>
     <column-list :list="list"></column-list>
   </div>
 </template>
@@ -8,6 +9,8 @@
 import { defineComponent } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ColumnList, { ColumnProps } from './components/ColumnList.vue'
+import GlobalHeader, { UserProps } from './components/Header.vue'
+
 const mockListData = [
   {
     id: 1,
@@ -31,15 +34,22 @@ const mockListData = [
       'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
   }
 ]
+const user: UserProps = {
+  isLogin: false,
+  id: 1,
+  name: 'jizai'
+}
 export default defineComponent({
   name: 'App',
   components: {
-    ColumnList
+    ColumnList,
+    GlobalHeader
   },
   setup () {
     const list: ColumnProps[] = mockListData
     return {
-      list
+      list,
+      user
     }
   }
 })
--- zhihu-ts/src/components/Header.vue
+++ zhihu-ts/src/components/Header.vue
@@ -1,16 +1,20 @@
 <template>
-  <div class="globalHeader"></div>
+  <nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <span class="navbar-brand mb-0 h1">ZHIHU</span>
+  </div>
+</nav>
 </template>
 
 <script lang="ts">
 import { PropType } from 'vue'
-interface UserProps {
+export interface UserProps {
   isLogin: boolean
   name?: string
   id?: number
 }
 export default {
-  name: 'GlobalHeader',
+  name: 'Header',
   props: {
     user: {
       type: Object as PropType<UserProps>,
@@ -24,6 +28,4 @@ export default {
 </script>
 
 <style scoped>
-.globalHeader {
-}
 </style>
