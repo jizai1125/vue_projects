260604934c6878e2cba3d7cbaeb1c72e1789002e
jizai <1414589221@qq.com>
2022-05-10 20:27:59 +0800

    u

--- wanzhuan-vue3/src/main.js
+++ wanzhuan-vue3/src/main.js
@@ -2,5 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
-
-createApp(App).use(router).use(store).mount("#app");
+import Element3 from "element3";
+import "element3/lib/theme-chalk/index.css";
+createApp(App).use(router).use(store).use(Element3).mount("#app");
